8aa06c2af8432c01b4f1c11c4ddbc9460fba49a1
Waischbrot <92989284+Waischbrot@users.noreply.github.com>
2022-07-18 17:30:22 +0200

    Create UUIDSerializer.java

---
+++ UUIDSerializer.java
@@ -0,0 +1,16 @@
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public class UUIDSerializer {
+    public static byte[] convert(UUID uuid) {
+        return ByteBuffer.wrap(new byte[16])
+                .putLong(uuid.getMostSignificantBits())
+                .putLong(uuid.getLeastSignificantBits())
+                .array();
+    }
+
+    public static UUID convert(byte[] bytes) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    }
+}
